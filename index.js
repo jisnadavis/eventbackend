c46fc861c2b5d7c34c288bc67f5d791b03552204
jisnadavis <jisnadavis93@gmail.com>
2024-07-26 13:28:00 +0200

    change

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
-app.use(
-  cors({
-    origin: '*',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization'
-  })
-)
+app.use(cors())
 
 connectdb()
 
